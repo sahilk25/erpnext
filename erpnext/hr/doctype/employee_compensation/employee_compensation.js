89163ca479979ed22843f8ad231457fd3f1aa3f6
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2021-09-15 17:04:18 +0530

    Update erpnext/hr/doctype/employee_compensation/employee_compensation.js

--- erpnext/hr/doctype/employee_compensation/employee_compensation.js
+++ erpnext/hr/doctype/employee_compensation/employee_compensation.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Compensation', {
-	// refresh: function(frm) {
-
-	// }
+	validate: function(frm) {
+		if (frm.doc.date_of_injury > frappe.datetime.get_today()) {
+			frappe.throw(__("You cannot select a future date for Date of Injury"));
+		}
+		if (frm.doc.first_aid_date > frappe.datetime.get_today()) {
+			frappe.throw(__("You cannot select a future date for First Aid Date"));
+		}
+		if (frm.doc.first_aid_date < frm.doc.date_of_injury) {
+			frappe.throw(__("The First Aid date cannot be before the Date of Injury"));
+		}
+	}
 });
